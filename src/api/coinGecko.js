95bd4793f66c1793457ff55f2f94d2437af87784
Nallamekala.Harshanvitha <Nallamekala.harshanvitha@accionlabs.com>
2026-09-23 11:42:16 +0530

    commit

--- src/api/coinGecko.js
+++ src/api/coinGecko.js
@@ -46,35 +46,6 @@ export const createCoin = (coin) =>
     method: "POST",
     body: JSON.stringify(coin),
   });
-  await createCoin({
-        id: "bitcoin",
-        symbol: "btc",
-        name: "Bitcoin",
-        image: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
-        current_price: 85347,
-        market_cap: 1714662023610,
-        market_cap_rank: 1,
-        fully_diluted_valuation: 1714662023610,
-        total_volume: 62659032437,
-        high_24h: 87330,
-        low_24h: 81460,
-        price_change_24h: 3727.44,
-        price_change_percentage_24h: 4.56682,
-        market_cap_change_24h: 75139395566,
-        market_cap_change_percentage_24h: 4.583,
-        circulating_supply: 20087781.0,
-        total_supply: 20087781.0,
-        max_supply: 21000000.0,
-        ath: 126080,
-        ath_change_percentage: -32.3069,
-        ath_date: "2025-10-06T10:57:42.000Z",
-        atl: 67.81,
-        atl_change_percentage: 125764.39361,
-        atl_date: "2013-07-05T16:00:00.000Z",
-        roi: null,
-        last_updated: "2026-09-22T06:58:10.000Z"
-    },
-  )
 
 export const updateCoin = (id, coin) =>
   request(`${BASE_URL}/coins/${id}`, {
